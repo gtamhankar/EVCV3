68b5125e6e5473ba809780c43bef3f7bd556d9d9
Gouri <gtamhankar@gmail.com>
2019-02-21 10:59:36 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+//mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+
+if (process.env.MONGODB_URI)
+{
+	// executes from heroku
+	mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+}
+else
+{
+	// executes from local machine
+	mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+}
 
 module.exports.User = require('./user');
 module.exports.Poll = require('./poll');
